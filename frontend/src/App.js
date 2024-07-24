d296db7a6615fb94d7adf4ac61b903add0cf583f
Samuel Adams <sadams@brb.state.tx.us>
2024-07-24 09:25:08 -0500

    created weight loss calculator form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
-import Header from "./component/Header.js";
-
+import Header from "./component/Header";
+import { EntryForm } from "./component/calculator/EntryForm";
 
 function App() {
   return (
     <div>
       <Header />
-      <div className="App-body">
+      <div className="m-5">
+        <EntryForm />
       </div>
     </div>
   );
