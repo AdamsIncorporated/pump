71f7e6216927b50dad0499cf21de6a7f300172e3
Sam Adams <samuel.grant.adams@gmail.com>
2025-04-03 20:47:07 -0500

    got the grid working probaly has some basic clipping bugs I need to deep dive on

--- frontend/src/components/grid.tsx
+++ frontend/src/components/grid.tsx
@@ -2,27 +2,52 @@ import React from "react";
 import { PanelGroup, Panel, PanelResizeHandle } from "react-resizable-panels";
 import LineChart from "./lineChart";
 
+// Reusable Panel component
+const DataPanel = ({ title, data, bgColor, chartTitle }) => {
+  return (
+    <Panel minSize={20} className="border-x-2">
+      <div className="flex group">
+        <div
+          className={`bg-${bgColor}-200 group-hover:${bgColor}-300 duration-300 ease-in w-fit text-black font-bold px-5`}
+        >
+          {title}
+        </div>
+        <div
+          className={`bg-${bgColor}-100 group-hover:${bgColor}-200 duration-300 ease-in w-1/2 rounded-l-none rounded-r-full`}
+
+        ></div>
+      </div>
+      <div className="m-5">
+        <LineChart chartTitle={chartTitle} data={data} />
+      </div>
+    </Panel>
+  );
+};
+
 export default function Grid() {
   return (
-    <div className="h-screen border">
+    <div className="h-screen">
       <PanelGroup autoSaveId="persistence" direction="horizontal">
-        <Panel minSize={20} className="border border-2-white p-2">
-          Calories
-          <div className="my-5">
-            <LineChart chartTitle="Calories" data={[1, 2, 3, 4]} />
-          </div>
-        </Panel>
+        <DataPanel
+          title="Calories"
+          data={[1, 2, 3, 4]}
+          bgColor="red"
+          chartTitle="Calories"
+        />
         <PanelResizeHandle />
-        <Panel minSize={20} className="border border-2-white p-2">
-          Weight
-          <div className="my-5">
-            <LineChart chartTitle="Weight (lbs)" data={[67, 70, 65, 80]} />
-          </div>
-        </Panel>
+        <DataPanel
+          title="Weight"
+          data={[67, 70, 65, 80]}
+          bgColor="blue"
+          chartTitle="Weight (lbs)"
+        />
         <PanelResizeHandle />
-        <Panel minSize={20} className="border border-2-white p-2">
-          Lift
-        </Panel>
+        <DataPanel
+          title="Lift"
+          data={[]}
+          bgColor="yellow"
+          chartTitle="Lift"
+        />
       </PanelGroup>
     </div>
   );
