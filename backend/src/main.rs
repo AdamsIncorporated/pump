3edf4b94c5e90b52443b83f952983421319890dd
Sam Adams <samuel.grant.adams@gmail.com>
2025-04-06 11:38:23 -0500

    fuckiing hate rust

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,14 +1,16 @@
 use actix_web::{App, HttpServer};
-use env_logger::Env;
 mod cors;
 mod db;
 use actix_web::middleware::Logger;
 mod handlers;
 use handlers::create::create;
+use log::info;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::init_from_env(Env::default().default_filter_or("info"));
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    info!("Starting Actix Web server...");
+
     HttpServer::new(|| {
         let cors = cors::create_cors();
         App::new()
