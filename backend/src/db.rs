c327683874693e47de9413fd914593a9b35b1186
Sam Adams <samuel.grant.adams@gmail.com>
2025-04-14 15:46:24 -0500

    getting warmer on db methods

--- backend/src/db.rs
+++ backend/src/db.rs
@@ -1,11 +1,11 @@
-use rusqlite::{Connection, MappedRows, ToSql};
 use log::error;
+use rusqlite::{Connection, ToSql};
 use serde::de::DeserializeOwned;
+use serde_rusqlite::from_rows;
 
 pub struct Database {
     conn: Connection,
 }
-
 
 impl Database {
     pub fn new() -> Result<Self, Box<dyn std::error::Error>> {
@@ -18,23 +18,33 @@ impl Database {
         }
     }
 
-    pub fn execute_sql(&mut self, sql: &String, params: &[&dyn ToSql]) -> Result<usize, Box<dyn std::error::Error>> {
+    pub fn execute_sql(
+        &mut self,
+        sql: &String,
+        params: &[&dyn ToSql],
+    ) -> Result<usize, Box<dyn std::error::Error>> {
         let tx = self.conn.transaction()?;
         let rows_affected = tx.execute(sql, params)?;
         tx.commit();
         Ok(rows_affected)
     }
 
-    pub fn read_rows<T: DeserializeOwned>(&mut self, sql: &String, params: &[&dyn ToSql]) -> Result<Vec<T>, Box<dyn std::error::Error>> {
+    pub fn read_rows<T: DeserializeOwned>(
+        &mut self,
+        sql: &String,
+        params: &[&dyn ToSql],
+    ) -> Result<Vec<T>, Box<dyn std::error::Error>> {
         let stmt = self.conn.prepare(sql)?;
         let mut results = Vec::new();
-        let rows = stmt
-            .query_map(params, |row| Ok(serde_rusqlite::from_row::<T>(row)));
-        for row in results {
-            results.push(row)
+        let rows = stmt.query_map(params, |row| Ok(serde_rusqlite::from_row::<T>(row)))?;
+
+        while let row = rows.next() {
+            match serde_rusqlite::from_row::<T>(row) {
+                Ok(item) => results.push(item),
+                Err(e) => return Err(e.into()), // Or handle the error as needed
+            }
         }
 
-        Ok(results)
-
+        Ok(rows)
     }
 }
