aba531c4aa23652918d39ceafbfa002827e17486
Sam Adams <samuel.grant.adams@gmail.com>
2025-06-27 11:47:07 -0500

    refactor Navbar component for improved styling and layout

--- frontend/src/components/nav.tsx
+++ frontend/src/components/nav.tsx
@@ -2,17 +2,9 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <nav className="bg-slate-600 text-white px-6 py-4 flex justify-between items-center mb-10">
-      <div className="text-xl font-bold tracking-wide">
-        PUMP
-      </div>
-      <div>
-        <button
-          type="button"
-          className="bg-slate-700 font-semibold px-4 py-2 rounded hover:bg-slate-800 transition"
-        >
-          Login
-        </button>
+    <nav className="bg-slate-600 text-white px-6 py-4 flex justify-between items-center shadow-md">
+      <div className="text-4xl font-bold tracking-wide">
+        PUMP 🥇
       </div>
     </nav>
   );
